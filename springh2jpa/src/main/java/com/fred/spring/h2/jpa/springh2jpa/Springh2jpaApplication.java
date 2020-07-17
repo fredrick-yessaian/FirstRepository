c4f26432be02c2402e533a1f559ce307c04a5e61
Joys Fredrick <Joys Fredrick@LAPTOP-KPN31IFM>
2020-07-17 11:43:26 -0400

    First Commit

---
+++ springh2jpa/src/main/java/com/fred/spring/h2/jpa/springh2jpa/Springh2jpaApplication.java
@@ -0,0 +1,13 @@
+package com.fred.spring.h2.jpa.springh2jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springh2jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springh2jpaApplication.class, args);
+	}
+
+}
---
+++ springh2jpa/src/test/java/com/fred/spring/h2/jpa/springh2jpa/Springh2jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fred.spring.h2.jpa.springh2jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springh2jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
